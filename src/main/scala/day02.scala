b3da08d637da22a76810a6c25c179d0fd9fa908c
niels prins <niels.prins@gmail.com>
2021-12-02 10:15:38 +0100

    day 2

---
+++ src/main/scala/day02.scala
@@ -0,0 +1,44 @@
+object Day2 extends App:
+
+  case class Vec2(depth: Int, horizon: Int):
+    lazy val multiple: Int = horizon * depth
+
+  case class Vec3(depth: Int, horizon: Int, aim: Int):
+    lazy val multiple: Int = horizon * depth
+
+  trait Move
+  case class Forward(v: Int) extends Move
+  case class Up(v: Int) extends Move
+  case class Down(v: Int) extends Move
+
+  def parse(i: String): Move = i match
+    case s"forward ${v}" => Forward(v.toInt)
+    case s"up ${v}"      => Up(v.toInt)
+    case s"down ${v}"    => Down(v.toInt)
+
+  def move(s: Vec2, m: Move): Vec2 =
+    m match
+      case Up(v)      => s.copy(depth = s.depth - v)
+      case Down(v)    => s.copy(depth = s.depth + v)
+      case Forward(v) => s.copy(horizon = s.horizon + v)
+
+  def move(s: Vec3, m: Move): Vec3 =
+    m match
+      case Up(v)   => s.copy(aim = s.aim - v)
+      case Down(v) => s.copy(aim = s.aim + v)
+      case Forward(v) =>
+        s.copy(horizon = s.horizon + v, depth = s.depth + s.aim * v)
+
+  println(
+    "day2".live
+      .map(parse)
+      .foldLeft(Vec2(0, 0))(move)
+      .multiple
+  )
+
+  println(
+    "day2".live
+      .map(parse)
+      .foldLeft(Vec3(0, 0, 0))(move)
+      .multiple
+  )
