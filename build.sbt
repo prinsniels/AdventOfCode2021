05990957435c4113b524c24508c321594d4fcbf0
niels prins <niels.prins@gmail.com>
2021-12-03 12:54:32 +0100

    day 3

--- build.sbt
+++ build.sbt
@@ -2,6 +2,15 @@
 ThisBuild / organization := "com.github.prinsniels"
 ThisBuild / scalaVersion := "3.1.0"
 
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-unchecked",
+  "-language:implicitConversions",
+  "-language:higherKinds",
+  "-language:existentials",
+  "-language:postfixOps"
+)
+
 
 lazy val aoc = project
 	.in(file("."))
---
+++ src/main/scala/day03.scala
@@ -0,0 +1,31 @@
+object day3 extends App:
+
+  def addBitString(acc: Array[Int], input: String): Array[Int] =
+    acc.zip(input.toCharArray.map(_.toString.toInt)).map(_ + _)
+
+  lazy val mostCommonBitPositions: List[String] => String = data =>
+    val summedPositions =
+      data.foldLeft(Array.fill(data.head.size)(0))((acc, nxtLine) =>
+        addBitString(acc, nxtLine)
+      )
+    summedPositions.map(x => if (x < data.length / 2.0) 0 else 1).mkString
+
+  lazy val leastCommonBitPositions: List[String] => String = data =>
+    mostCommonBitPositions(data).map(x => if (x == '1') '0' else '1')
+
+  def binToInt(inp: String): Int =
+    Integer.parseInt(inp, 2)
+
+  val mostCommon = mostCommonBitPositions("day3".live.toList)
+  val leastCommon = leastCommonBitPositions("day3".live.toList)
+
+  println(Integer.parseInt(mostCommon, 2) * Integer.parseInt(leastCommon, 2))
+
+  def O(events: List[String], f: List[String] => String): Int =
+    def search(remainder: List[String], pos: Int): Int =
+      if (remainder.length == 1) Integer.parseInt(remainder.head, 2)
+      else {
+        search(remainder.filter(_.charAt(pos) == f(remainder).charAt(pos)), pos + 1)}
+    search(events, 0)
+
+  println(O("day3".live.toList, mostCommonBitPositions) * O("day3".live.toList, leastCommonBitPositions))
