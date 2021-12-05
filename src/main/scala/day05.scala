0e021ab48405680faffc8cbfecaed48a6c4977ac
niels prins <niels.prins@gmail.com>
2021-12-05 10:24:50 +0100

    minor code genreralization

--- src/main/scala/day05.scala
+++ src/main/scala/day05.scala
@@ -7,15 +7,13 @@ object day05 extends App:
     Point(x1.toInt, y1.toInt) -> Point(x2.toInt, y2.toInt)
   }
 
-  def toPointMap(p1: Point, p2: Point): Map[Point, Int] =
-    val xd = if (p1.x < p2.x) 1 else if (p1.x == p2.x) 0 else -1
-    val yd = if (p1.y < p2.y) 1 else if (p1.y == p2.y) 0 else -1
-    
+  val deltaValue: (Int, Int) => Int = (l,r) => if (l < r) 1 else if (l == r) 0 else -1
+  
+  def toPoints(p1: Point, p2: Point): List[Point] =
     def helper(cur: Point, delta: Point, acc: List[Point]): List[Point] =
       if (cur == p2) cur :: acc
       else helper( cur + delta, delta, cur :: acc)
-
-    helper(p1, Point(xd, yd), List.empty[Point]).map(_ -> 1).toMap
+    helper(p1, Point(deltaValue(p1.x, p2.x), deltaValue(p1.y, p2.y)), List.empty[Point])
 
   def merge[A](m1: Map[A, Int], m2: Map[A, Int]): Map[A, Int] =
     (m1 ++ m2.map((k, v) => k -> (m1.getOrElse(k, 0) + v)))
@@ -24,15 +22,15 @@ object day05 extends App:
     "day5".live
       .map(parse)
       .filter((l, r) => l.x == r.x || l.y == r.y)
-      .map(toPointMap)
+      .map((x, y) => toPoints(x, y).map(_ -> 1).toMap)
       .reduce(merge)
       .filter((p, i) => i >= 2)
       .size
   )
   println(
-    "day5".live
+    "day5".tst
       .map(parse)
-      .map(toPointMap)
+      .map((x, y) => toPoints(x, y).map(_ -> 1).toMap)
       .reduce(merge)
       .filter((p, i) => i >= 2)
       .size
