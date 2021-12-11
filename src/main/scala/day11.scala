b02fd78fada56816a02024c76753c0b4e838d349
niels prins <niels.prins@gmail.com>
2021-12-11 11:13:43 +0100

    Map[Vec, Int] -> Board[Int]

--- src/main/scala/day11.scala
+++ src/main/scala/day11.scala
@@ -2,11 +2,11 @@
 object day11 extends App:
   import BoardUtils.* 
 
-  lazy val field: Map[Vec, Int] =
+  lazy val field: Board[Int] =
     "day11".live.zipWithIndex.flatMap((line, y) => line.zipWithIndex.map((c, x) => Vec(x, y) -> c.asDigit)).toMap
 
-  def transform(field: Map[Vec, Int]): Map[Vec, Int] =
-    def helper(pointMap: Map[Vec, Int], activated: Set[Vec]): (Map[Vec, Int]) =
+  def transform(field: Board[Int]): Board[Int] =
+    def helper(pointMap: Board[Int], activated: Set[Vec]): (Board[Int]) =
       pointMap.filter((k, v) => (v > 9) & !activated(k)) match
         case inc if inc.isEmpty => pointMap
         case inc => {
@@ -16,7 +16,7 @@ object day11 extends App:
         }
     helper(field.map((k, v) => k -> (v + 1)), Set.empty[Vec]).map((k, v) => k -> (if (v > 9) 0 else v))
 
-  def transformFor(f: Map[Vec, Int], steps: Int) = 
+  def transformFor(f: Board[Int], steps: Int) = 
      (1 to steps).foldLeft((f, 0)) {
          (acc, _) => acc match 
              case (field, flashes) =>  
@@ -24,12 +24,12 @@ object day11 extends App:
                 nw -> (nw.count((_, v) => v == 0) + flashes)
      }
 
-  def firstAll(f: Map[Vec, Int]): Int =
-    def helper(f: Map[Vec, Int], c: Int, pred: Map[Vec, Int] => Boolean): Int = 
+  def firstAll(f: Board[Int]): Int =
+    def helper(f: Board[Int], c: Int, pred: Board[Int] => Boolean): Int = 
         val nw = transform(f)
         if (pred(nw)) c
         else helper(nw, c + 1,  pred)
-    helper(f, 0, (x: Map[Vec, Int]) => x.count((_, v) => v > 0) == 0) + 1
+    helper(f, 0, (x: Board[Int]) => x.count((_, v) => v > 0) == 0) + 1
 
   transformFor(field, 100)._2 andThenShowWith "ex1"
 
